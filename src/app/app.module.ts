25431eb74431e2da7134c9ead9072bbe9ba5b1a1
kalaivani <2020kalaivanin@gmail.com>
2023-09-19 13:03:13 +0530

    initial setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CompanyComponent } from './company/company.component';
+import { UsageComponent } from './usage/usage.component';
+import { ResultsComponent } from './results/results.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CompanyComponent,
+    UsageComponent,
+    ResultsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
